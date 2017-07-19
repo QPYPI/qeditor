b39c85ccedd314ebf9b53be17587b6db4ac99936
hm <bugreject@gmail.com>
2017-07-19 15:18:34 +0800

    New version for QPy2.0

--- src/main/assets/mbox/act.js
+++
@@ -1,6 +0,0 @@
-{
-    $('#mainEle').html('<div id="loading">Welcome to use QEdit browser</div>');
-
-   /* $("<scri"+"pt>"+"").attr({src:host+"/mbox/md3.js", type:'text/javascript',id:'load'}).appendTo($('head'));*/
-
-}
--- src/main/java/com/quseit/texteditor/FTPServerService.java
+++
@@ -1,9 +0,0 @@
-package com.quseit.texteditor;
-
-public class FTPServerService extends org.swiftp.FTPServerService {
-
-	@Override
-	protected Class<?> getSettingClass() {
-		return MFTPSettingActivity.class;
-	}
-}
